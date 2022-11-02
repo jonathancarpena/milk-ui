514cd910e218bf27366ccbb6e4271b8d6af7ef6b
Jonathan <jonathancarpena99@gmail.com>
2022-11-01 18:58:49 -0700

    Fixed mobile active styles on homepage

--- pages/components/App/Home/Banner.tsx
+++ pages/components/App/Home/Banner.tsx
@@ -3,17 +3,19 @@ import { SiTailwindcss, SiReact } from 'react-icons/si';
 function Banner() {
 	return (
 		<section className="text-center max-w-lg mx-auto flex flex-col    py-32">
-			<h1 className="font-semibold text-8xl font-design ">MilkUI</h1>
+			<h1 className="font-semibold text-8xl font-design tracking-tight ">
+				MilkUI
+			</h1>
 			<p className=" text-stone-500">
 				Free Application UI Tailwind CSS Components
 			</p>
 			<p className="mt-2 text-sm text-stone-500">
 				MilkUI is a collection of free Tailwind CSS components that can be used
-				in your next React project. With a range of components, you can build
-				with ease.
+				in your next React project. With a list of responsive components, you
+				can build with ease.
 			</p>
 
-			<p className="text-sm mt-3 flex space-x-1 items-center mx-auto text-stone-500 bg-white p-3 rouned-sm shadow-md">
+			<p className="text-sm mt-3 flex space-x-1 items-center mx-auto text-stone-500 bg-white py-3 px-4 rounded-full shadow-md">
 				<span>Powered by:</span>
 				<span className="flex space-x-1 items-center ">
 					<span>TailwindCSS</span>
--- pages/components/App/Home/Grid.tsx
+++ pages/components/App/Home/Grid.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { sampleComponents } from '../../../../lib/utils';
 
 type Props = {
@@ -6,16 +5,15 @@ type Props = {
 };
 
 function GridItem({ icon, name, components }: Props['GridItem']) {
-	const [hover, setHover] = useState(false);
 	return (
-		<li
-			onMouseEnter={() => setHover(true)}
-			onMouseLeave={() => setHover(false)}
-			className="select-none cursor-pointer flex flex-col space-y-3 bg-white shadow-md border-1 p-4  hover:-translate-y-4  transition-transform ease-linear duation-150"
-		>
-			<span className="text-lg">{icon}</span>
-			<p className="capitalize text-lg font-semibold">{name}</p>
-			<p className="text-sm text-stone-500">{components} Components</p>
+		<li className=" relative rounded-3xl select-none cursor-pointer  active:bg-neutral-500 lg:hover:bg-neutral-500 transition-all ease-out ">
+			<div className="bg-white   active:bg-neutral-100 active:scale-75 lg:active:scale-100 lg:hover:-translate-y-5 lg:hover:translate-x-5 transition-transform ease-out duration-200 p-4 rounded-3xl border-2 ">
+				<p className="text-lg mb-2">{icon}</p>
+				<p className="capitalize text-lg font-semibold mb-2">{name}</p>
+				<p className="text-sm text-stone-500">
+					{components} Component{components > 1 ? 's' : ''}
+				</p>
+			</div>
 		</li>
 	);
 }
@@ -23,7 +21,7 @@ function GridItem({ icon, name, components }: Props['GridItem']) {
 function Grid() {
 	return (
 		<section className=" pb-32">
-			<ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
+			<ul className="grid grid-cols-2 md:grid-cols-3 gap-3 lg:grid-cols-4 lg:gap-6 ">
 				{sampleComponents.map((item) => (
 					<GridItem
 						key={item.name}
